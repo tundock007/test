7a6565791f5da41d6e51a79ab6c288c7eabe34be
ola <ola00@icloud.com>
2016-01-25 10:33:12 -0500

    test_repo

---
+++ Strings/src/Strings.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Strings.cpp
+// Author      : Test2
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string value = "Hello world, this is string";
+	string Name = " Olatunde";
+
+
+	cout << value + Name << endl;
+	return 0;
+}
