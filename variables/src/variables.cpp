39867c08a01013538c5f0a0f9847b49d9151fb70
Ola <Ola@192.168.1.104>
2016-01-25 11:11:16 -0500

    Variables test

---
+++ variables/src/variables.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : variables.cpp
+// Author      : ola
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int numberCats = 5;
+	int numberDogs = 7;
+
+	cout << "Number of cats: " << numberCats << endl; //
+	cout << "Number of cats: " << numberDogs << endl;
+	return 0;
+}
